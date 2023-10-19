7b7665a376f16de9e001c6e189a2adda8e826de3
Disnasudheer <119562836+Disnasudheer@users.noreply.github.com>
2023-10-19 22:47:49 +0530

    assignment

---
+++ D/js/imgresize1.js
@@ -0,0 +1,25 @@
+    function heightpx() {
+    let hpix = document.getElementById("hpx");
+    let image = document.getElementById("img");
+    let value = hpix.value + "px"
+    image.style.height = value;
+    }
+    function heightper() {
+    let hper = document.getElementById("hpercent");
+    let image = document.getElementById("img");
+    let value = hper.value + "%"
+    image.style.height = value;
+    }
+    function widthpx() {
+    let wpix = document.getElementById("wpx");
+    let image = document.getElementById("img");
+    let value = wpix.value + "px"
+    image.style.width = value;
+    }
+    function widthper() {
+    let wper = document.getElementById("wpercent");
+    let image = document.getElementById("img");
+    let value = wper.value + "%"
+    image.style.width = value;
+    }
+    
